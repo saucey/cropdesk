4f9ea0a3547a23ff2d5ab8ac9eeee780488a55e5
Leo Scott <leo@designanddev.co.uk>
2020-12-20 12:00:46 +0000

    create basic route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'games', component: GamesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HttpClientModule],
